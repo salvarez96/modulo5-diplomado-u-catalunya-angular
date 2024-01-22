72e4da80256a34353d15cddd3ea7d157c7d76644
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-01-22 14:17:37 -0500

    Create home and clientes components
    Add new routes for home and clientes components
    
    Erase app.component.ts information

--- angularapp/src/app/app.routes.ts
+++ angularapp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'clientes',
+    component: ClientesComponent
+  }
+];
